9604dc42f88ac54fe7ce4156e75498307261700a
djbeadle <djbeadle+github@gmail.com>
2018-02-04 13:05:08 -0500

    Very basic framework is working!

--- InhalerTrackerV2/ViewController.swift
+++ InhalerTrackerV2/ViewController.swift
@@ -9,16 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var outputDisplay: UITextField!
+    @IBOutlet weak var puffCounter: UIStepper!
+    
+    
+    private let authorizeHealthKitSection = 2
+    
+    private func authorizeHealthKit() {
+        HealthKitSetupAssistant.authorizeHealthKit { (authorized, error) in
+            guard authorized else {
+                let baseMessage = "HealthKit Authorization Failed"
+                if let error = error {
+                    print("\(baseMessage). Reason: \(error.localizedDescription)")
+                } else {
+                    print(baseMessage)
+                }
+                return
+            }
+            print("HealthKit Successfully Authorized.")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        authorizeHealthKit()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func puffCounter(_ sender: UIStepper) {
+        outputDisplay.text = Int(sender.value).description
+    }
+    
 
 
 }
