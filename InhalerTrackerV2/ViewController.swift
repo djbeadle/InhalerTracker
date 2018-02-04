ff187b2142f2e4de5a63a3da51bbbd9a37105427
djbeadle <djbeadle+github@gmail.com>
2018-02-03 22:06:04 -0500

    Initial Commit

---
+++ InhalerTrackerV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InhalerTrackerV2
+//
+//  Created by Daniel Beadle on 2/3/18.
+//  Copyright © 2018 Daniel Beadle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
